a1480dc67ea4786a0cbd13439b6d946f7088952a
Bence <bence.fogarasy@playtech.com>
2022-01-13 12:11:57 +0000

    Database handling works however issues with async and sqlite.

--- DatabaseProject/loadJsonInDatabase.js
+++ DatabaseProject/loadJsonInDatabase.js
@@ -6,67 +6,118 @@ const db = new sqlite3.Database('restaurants.sqlite');
 let stmt;
 
 let query2;
+let query3;
+
+
+stmt = db.prepare(`INSERT INTO Restaurant (name, imagelink) VALUES (?, ?);`);
+
+query2 = db.prepare(`INSERT INTO Menu (title, restaurant_id) VALUES (?, ?);`);
+
+query3 = db.prepare(`INSERT INTO MenuItem (name,price,menu_id) VALUES (?, ?, ?)`);
 
 async function load() { 
     
+    
+
     console.log('calling load');
     // wait for the file to be read
     const buffer = await fsp.readFile('./data.json');
     const restaurants = (JSON.parse(String(buffer)));
+    try{
     
-    
+        for ( arr in restaurants){
+            console.log("works unitl here1");
 
-    stmt = db.prepare(`INSERT INTO Restaurant (name, imagelink) VALUES (?, ?);`);
-    query2 = db.prepare(`INSERT INTO Menu (title, restaurant_id) VALUES (?, ?);`);
-    for ( arr in restaurants){
+            restaurantObject = restaurants[arr];
 
-        restaurantObject = restaurants[arr];
+            let name = restaurantObject.name;
+            let image = restaurantObject.image;
 
-        let name = restaurantObject.name;
-        let image = restaurantObject.image;
-
-        let menus = restaurantObject.menus;
+            let menus = restaurantObject.menus;
 
 
-        let menuItem = [];
-        let menu = [];
+            let menuItem = [];
+            let menu = [];
+            let menuID = 0;
 
-        //add restaurant
-        try {
-            db.serialize(function () { 
-               try {
+            //add restaurant
+            try {
+                db.serialize(function () { 
+                    try {
+                        
+                    
 
-                stmt.run([name,image]);
+                        stmt.run([name,image]);
+                            //insert restaurant
+                        console.log("works unitl here");
+                db.get("SELECT id FROM Restaurant WHERE name=?",[name], (err, row)=>{
+                    
+                    console.log(row);
+                        for (m in menus){
+                            restaurantID = row.id;
+                            try{
+                                if (err)
+                                    console.error(err);
 
-               } finally {
-                   stmt.finalize();
-               }
-               db.get("SELECT id FROM Restaurant WHERE name=?",[name], (err, row)=>{
-                    for (m in menus){
-                        restaurantID = row.id;
-                        try{
-                            console.error(err);
-                            console.log(restaurantID);
-                            query2.run([menus[m].title,restaurantID]);
-                        }finally{
-                            query2.finalize();
+                                query2 = db.prepare(`INSERT INTO Menu (title, restaurant_id) VALUES (?, ?);`);
+                                query2.run([menus[m].title,restaurantID]);
+
+                                for (mI in menus[m].items){
+
+                                    menuItem = (menus[m].items[mI]);
+                                    let currentMenuItemName = menuItem.name;
+                                    let currentMenuItemPrice = menuItem.price;
+                                   
+                                    db.get("SELECT id FROM Menu WHERE title=?",[menus[m].title], (erro, menuRow)=>{
+                                        menuID = menuRow.id;
+                                        console.log(currentMenuItemName+ "  -   "+ currentMenuItemPrice + "  -   "+ menuID);
+                                        try{
+                                            query3 = db.prepare(`INSERT INTO MenuItem (name,price,menu_id) VALUES (?, ?, ?)`);
+                                            query3.run([currentMenuItemName,currentMenuItemPrice,menuID]);
+                                        }catch(dbError1){
+                                            console.error(dbError1);
+                                        }
+                                    });
+                                    
+                                }
+                            }catch(err){
+                                console.error(err);
+                            }
                         }
-                    }
+                    });
+                    
+                    
+                }catch(err){
+                    console.error(err);
+                } finally {
+
+                }
                 });
-                
-               
-           });
-       } finally {
-           // release resources 
-             
-       }
-        return;
+            } finally {
+            }
+        
+        
+            // return;
+        }
+    }finally{
+        
     }
-    db.close();
+        
+    
+    
+    
 }
 
-
-load();
+(async() => {
+    try{
+        await load();
+    }finally{
+        stmt.finalize();  
+        query2.finalize();   
+        query3.finalize(); 
+        db.close();
+    }
+})();
 
 
 
