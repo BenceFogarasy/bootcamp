500283733eb03c2b9434b74879bd0a29f661aede
Bence <bence.fogarasy@playtech.com>
2022-01-17 11:24:48 +0000

    server example project

---
+++ Sequalize/Restaurant.js
@@ -0,0 +1,26 @@
+const {sequelize, DataTypes, Model} = require('./sequelize_index');
+const {Menu} = require('./Menu')
+/**
+ * Represents a Restaurant
+ */
+class Restaurant extends Model {
+
+
+
+}
+
+Restaurant.init({
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+}, {
+    sequelize,
+    timestamps: false,
+});
+
+Restaurant.hasMany(Menu, {as: 'menus', foreignKey: 'restaurant_id'})
+Menu.belongsTo(Restaurant, {foreignKey: 'restaurant_id'})
+
+
+module.exports = {
+    Restaurant
+};
